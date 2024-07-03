9fd54d0afd91017e881066ffc52b38a5eb1a80a3
SingularMJ <67386371+SingularMJ@users.noreply.github.com>
2024-07-03 02:37:16 -0300

    updates
    changed the colors on the hud life bar and special bar (placeholders)
    
    the hud bars now have a bar appearance to help visualising (placeholder)

--- Sonic Battle Prototype/Scripts/hud.gd
+++ Sonic Battle Prototype/Scripts/hud.gd
@@ -16,12 +16,14 @@ func update_hud(life_amount, special_amount, points_amount):
 
 ## change the amount shown in the life points on hud in-game
 func change_life(life_amount):
-	life_ui.text = str(life_amount)
+	var new_life_bar = fill_bar_with(life_amount)
+	life_ui.text = new_life_bar
 
 
 ## change the amount shown in the special bar on hud in-game
 func change_special(special_amount):
-	special_ui.text = str(special_amount)
+	var new_special_bar = fill_bar_with(special_amount)
+	special_ui.text = new_special_bar
 
 
 ## change the amount of points gained shown on hud in-game
@@ -32,3 +34,18 @@ func change_points(points_amount):
 ## change the avatar image next to the points shown on hud in-game
 func change_avatar(new_avatar):
 	avatar_ui.texture = new_avatar
+
+
+func fill_bar_with(resource) -> String:
+	var new_bar = "/"
+	# add a char for each 4 points
+	var char_amount = resource / 4
+	for i in range(char_amount):
+		new_bar += "|"
+	
+	# fill blank spaces
+	var empty = (100 - resource) / 4
+	for j in range(empty):
+		new_bar += "_"
+	new_bar += "/"
+	return new_bar
