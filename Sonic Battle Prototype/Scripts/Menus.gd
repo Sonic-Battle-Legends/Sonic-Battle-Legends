952f2be6096d0e3a5d8e8b3bce3a295d78e56403
SingularMJ <67386371+SingularMJ@users.noreply.github.com>
2024-07-17 02:09:20 -0300

    updates
    added difficulty levels on options to delay the bot response accordingly.

--- Sonic Battle Prototype/Scripts/Menus.gd
+++ Sonic Battle Prototype/Scripts/Menus.gd
@@ -16,6 +16,7 @@ extends Control
 @export var mode_selection_menu: Control
 @export var character_selection_menu: Control
 @export var area_selection_menu: Control
+@export var pause_menu: Control
 
 @export_category("SERVER CANVAS MENU")
 @export var canvas_server_menu: Node3D
@@ -57,6 +58,7 @@ func hide_menus():
 	mode_selection_menu.hide()
 	character_selection_menu.hide()
 	area_selection_menu.hide()
+	pause_menu.hide()
 
 
 ## proceed with the normal screens sequence after online setup
@@ -98,7 +100,11 @@ func _on_quit_button_pressed():
 
 func _on_options_back_button_pressed():
 	hide_menus()
-	main_menu.show()
+	# return to pause menu if the game is was paused
+	if GlobalVariables.current_character != null:
+		pause_menu.show()
+	else:
+		main_menu.show()
 
 
 func _on_online_button_pressed():
@@ -182,3 +188,10 @@ func _on_back_button_pressed():
 		hide_menus()
 		character_selection_menu.show()
 
+
+
+func _on_difficulty_selection_list_item_selected(index):
+	# select the difficulty fro the enumerator
+	var difficulty_selected = GlobalVariables.difficulty_levels.find_key(index)
+	# set the current difficulty
+	GlobalVariables.current_difficulty = GlobalVariables.difficulty[str(difficulty_selected)]
